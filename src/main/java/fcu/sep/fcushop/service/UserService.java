9718941124d05abbafc0c018120e0714a75d0663
kamiki <c.s.f.kamiki@mail.fcu.edu.tw>
2021-12-19 00:33:08 +0800

    get user password service

--- src/main/java/fcu/sep/fcushop/service/UserService.java
+++ src/main/java/fcu/sep/fcushop/service/UserService.java
@@ -1,14 +1,28 @@
 package fcu.sep.fcushop.service;
 
+import fcu.sep.fcushop.database.Sql2oDbHandler;
+import fcu.sep.fcushop.model.Product;
 import fcu.sep.fcushop.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.sql2o.Connection;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
-    
 
+    @Autowired
+    private Sql2oDbHandler sql2oDbHandler;
 
+    public String getUserPassword(String account) {
+        try (Connection connection = sql2oDbHandler.getConnector().open()) {
+            String query = "select PASSWORD"
+                + " from USER where ACCOUNT like :account";
+            return connection.createQuery(query)
+                    .addParameter("account", account)
+                    .toString();
+        }
+    }
 }
