704654b3eb57d39127b03b7cb2a7a3e6173220c8
kamiki <c.s.f.kamiki@mail.fcu.edu.tw>
2021-12-18 20:46:15 +0800

    Open User Service

---
+++ src/main/java/fcu/sep/fcushop/service/UserService.java
@@ -0,0 +1,14 @@
+package fcu.sep.fcushop.service;
+
+import fcu.sep.fcushop.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    
+
+
+}
